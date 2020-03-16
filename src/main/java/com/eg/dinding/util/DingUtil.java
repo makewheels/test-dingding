00ba237f50da599fff5c94e8d99ac86777be78eb
makewheels <33256842+makewheels@users.noreply.github.com>
2020-03-16 19:43:44 +0800

    webhookUrl+secret配置

--- src/main/java/com/eg/dinding/util/DingUtil.java
+++ src/main/java/com/eg/dinding/util/DingUtil.java
@@ -12,9 +12,12 @@ import javax.crypto.spec.SecretKeySpec;
 import java.net.URLEncoder;
 
 /**
+ * 钉钉机器人工具类
+ *
  * @time 2020-03-16 18:22
  */
 public class DingUtil {
+    private final static String webhookUrl = "https://oapi.dingtalk.com/robot/send?access_token=df52b12e37f29cdd50dc64ca0fa0472d28f144f8fde14bb3a62f1224aab50a3b";
     private final static String secret = "SECa9c39f7948dd28271a651b0dfb60d5c86b028c49f85b79d79b8522e4f2e27e13";
 
     public static String getUrl() {
@@ -26,9 +29,7 @@ public class DingUtil {
             mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
             byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
             String sign = URLEncoder.encode(new String(new Base64().encode(signData)), "UTF-8");
-            return "https://oapi.dingtalk.com/robot/send?" +
-                    "access_token=df52b12e37f29cdd50dc64ca0fa0472d28f144f8fde14bb3a62f1224aab50a3b"
-                    + "&timestamp=" + timestamp + "&sign=" + sign;
+            return webhookUrl + "&timestamp=" + timestamp + "&sign=" + sign;
         } catch (Exception e) {
             e.printStackTrace();
         }
